2881ee0363b89fc84f7c68cd43ba52aa30b07d43
Sri Varsha <CaptainVarsha@Sris-MacBook-Pro.local>
2015-10-02 14:54:14 -0500

    code pushed

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+app.use(express.static(__dirname  + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+
+
+var quotes = [
+  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
+  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
+  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
+  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+];
+
+app.listen(process.env.PORT || 5000);
+
+app.get('/', function(request, response)
+{
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/quotes', function(req, res) {
+  res.json(quotes);
 });
-
-
