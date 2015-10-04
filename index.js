e1ac637c61b20d910f88b32e1379a39e8920c85e
Sri Varsha <CaptainVarsha@Sris-MacBook-Pro.local>
2015-10-04 10:13:41 -0500

    code pushed

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ app.set('view engine', 'ejs');
 
 
 var quotes = [
-  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
-  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
-  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
-  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+  { author : 'Alexander Graham Bell', text : "When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one that has opened for us."},
+  { author : 'Erica Jong',            text : "Advice is what we ask for when we already know the answer but wish we didn't."},
+  { author : 'Unknown',               text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
+  { author : 'Neale Donald Walsch',   text : "You are afraid to die, and you're afraid to live. What a way to exist."},
+  { author : 'Dr. Seuss',             text : "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind."},
+  { author : 'Lou Holtz',             text : "Never tell your problems to anyone...20% don't care and the other 80% are glad you have them."},
+  { author : ' Unknown',              text : "When life gives you a hundred reasons to cry, show life that you have a thousand reasons to smile."},
+  { author : 'shing xiong',           text : "In the end, it's not going to matter how many breaths you took, but how many moments took your breath away"},
+  { author : 'Margaret Mead',         text : "Always remember that you are absolutely unique. Just like everyone else."},
+  { author : 'Oscar Wilde',           text : "Be yourself; everyone else is already taken."}
+
+
 ];
 
 app.listen(process.env.PORT || 5000);
